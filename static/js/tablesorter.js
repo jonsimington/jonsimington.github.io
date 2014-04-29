d76aa62888554f029860b2536e1be4097b558d83
jswv9@mst.edu <jswv9@mst.edu>
2014-04-29 09:37:00 -0500

    Add sorting to stats table

---
+++ static/js/tablesorter.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+    {
+        $("#tourney_stats").tablesorter();
+    }
+);
+
+$(document).ready(function()
+    {
+        $("#tourney_stats").tablesorter( {sortList: [[0,0], [1,0], [2,0], [3,0]]} ); 
+    }
+);
